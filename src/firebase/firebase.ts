430f639692c661e70eda7ab565459cae2c9262bf
BLND-99 <blendmustafa71@gmail.com>
2025-10-01 04:00:34 -0400

    Minor changes

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,11 +1,11 @@
-// src/firebase/firebase.ts
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
-  // ...other config values from your Firebase project settings
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ src/firebase/postIncidents.tsx
@@ -0,0 +1,47 @@
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { useState } from 'react';
+
+const Firestore = () => {
+    const [location, Setlocation] = useState("");
+    const [incidentType, Setincidenttype] = useState("");
+    const [description, Setdescription] = useState("");
+    const sub = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        // Add data to the store
+        const firestore = getFirestore();
+        addDoc(collection(firestore, "data"), {
+            Location: location,
+            IncidentType: incidentType,
+            Description: description
+        })
+            .then(() => {
+                alert("Data Successfully Submitted");
+            })
+            .catch((error: any) => {
+                console.error("Error adding document: ", error);
+            });
+    }
+
+    return (
+        <div>
+            <center>
+                <form style={{ marginTop: "200px" }}
+                    onSubmit={(event) => { sub(event) }}>
+                    <input type="text" placeholder="your name"
+                        onChange={(e) => { Setlocation(e.target.value) }} />
+                    <br /><br />
+                    <input type="number" placeholder="your age"
+                        onChange={(e) => { Setincidenttype(e.target.value) }} />
+                    <br /><br />
+                    <input type="text" placeholder="Course Enrolled"
+                        onChange={(e) => { Setdescription(e.target.value) }} />
+                    <br /><br />
+                    <button type="submit">Submit</button>
+                </form>
+            </center>
+        </div>
+    );
+}
+
+export default Firestore;
