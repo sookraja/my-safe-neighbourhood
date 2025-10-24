eefad101567744ae869692ef9ef77bd67030b760
sookraja <sookraja@sheridancollege.ca>
2025-10-24 18:15:29 -0400

    Resolve Firebase config merge conflict

--- src/components/ui/LandingPage.tsx
+++ src/components/ui/LandingPage.tsx
@@ -204,7 +204,6 @@ const LandingPage: React.FC = () => {
                   )}
                 </div>
               </div>
-
               <div className="h-[400px]">
                 <RealMapComponent 
                   incidents={mockIncidents}
