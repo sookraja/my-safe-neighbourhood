ddac01700d62ffac14f58e4801941b9377391a63
sookraja <sookraja@sheridancollege.ca>
2025-11-04 00:18:10 -0500

    small updates

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZdVy9RJDYSNQJVDUYia94lNE6FpxmhbU",
-  authDomain: "mysafeneighbourhood.firebaseapp.com",
-  projectId: "mysafeneighbourhood",
-  storageBucket: "mysafeneighbourhood.firebasestorage.app",
-  messagingSenderId: "104149866468",
-  appId: "1:104149866468:web:f784e606d64df963efd9f1",
-  measurementId: "G-6FDX3STHNY"
+  
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+
 };
 
 const app = initializeApp(firebaseConfig);
