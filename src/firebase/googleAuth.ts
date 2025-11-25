8e8d74c8509af777a9efc4fb710b5e7a879a866a
Edgar Ponce <ponceed@sheridancollege.ca>
2025-11-25 14:36:26 -0500

    fixes

--- src/firebase/googleAuth.ts
+++ src/firebase/googleAuth.ts
@@ -6,12 +6,14 @@ import { db } from './firebase';
 const provider = new GoogleAuthProvider();
 
 export const signInWithGoogle = async () => {
+  //this opens the google sign in pop up
   const result = await signInWithPopup(auth, provider);
   const user = result.user;
-  const userDocRef = doc(db, 'Users', user.uid);
-  //im basically checking if the user already exists 
+   //checking if the user already exists/referencing document in firebase
+  const userDocRef = doc(db, 'Users', user.uid); 
   const userDoc = await getDoc(userDocRef);
 
+  //creating the user profile if its the first time signning in 
   if (!userDoc.exists()) {
     await setDoc(userDocRef, {
       email: user.email,                      
